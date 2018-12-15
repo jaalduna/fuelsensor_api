b31400b19c77bdbce7ee9a53119b096a39a19054
joaquin aldunate <jaalduna@gmail.com>
2018-12-15 15:19:35 -0300

    minor change

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ class Mock(MagicMock):
     @classmethod
     def __getattr__(cls,name):
             return MagicMock()
-MOCK_MODULES = ['crcmod','matplotlib']
+MOCK_MODULES = ['crcmod','matplotlib.pyplt']
 sys.modules.update((mod_name,Mock()) for mod_name in MOCK_MODULES)
 sys.path.insert(0, os.path.abspath('../src/'))
 
