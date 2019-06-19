1c37ad58b569ea2119cd7dbaca1921ef6e74ddef
joaquin aldunate <jaalduna@gmail.com>
2019-06-18 23:17:21 -0400

    deleted merge conflict text

--- src/get_hight_test.py
+++ src/get_hight_test.py
@@ -1,17 +1,10 @@
 import sys
 from Fuelsensor_interface import Fuelsensor_interface
 
-
-<<<<<<< HEAD
-from Fuelsensor_interface import Fuelsensor_interface
-fs = Fuelsensor_interface('192.168.0.100',5000)
-=======
 if len(sys.argv) == 3:
     fs = Fuelsensor_interface(str(sys.argv[1]), int(sys.argv[2]))
 else:
     fs = Fuelsensor_interface()
-    
->>>>>>> 06f9cbbc17af44e520c304c4856eeb3107e4fb0e
 print "conectando"
 fs.connect()
 print "solicitando altura"
