ce838f7509c6ef5963c371e75b181b68b3c56576
Aplication <Aplication@MTABCLLOB630.anglo.local>
2019-07-15 11:59:44 -0400

    minor changes

--- src/get_param.py
+++ src/get_param.py
@@ -13,21 +13,24 @@ params.sdft_i_min.interface.connect()
 params.sdft_i_min.get_value()
 print params.sdft_i_min.value
 
-params.sdft_i_min.set_value(200)
+params.sdft_i_min.set_value(1000)
 print params.sdft_i_min.value
 
 params.sdft_n.get_value()
 params.sdft_k.get_value()
 params.sdft_k.set_value(49)
-params.sdft_peak.get_value()
-params.pga_gain.set_value(2)
-params.num_pulses.set_value(0)
-params.num_pulses.get_value()
+params.sdft_peak.set_value(10)
+params.pga_gain.set_value(7)
+params.num_pulses.set_value(10)
+params.sdft_n_samples_one_valid.set_value(20)
+params.sdft_min_peak_value_th.get_value()
+params.sdft_min_peak_value_th.set_value(0.2)
 print "N:", params.sdft_n.value
 print "k:", params.sdft_k.value
 print "sdft paek: ", params.sdft_peak.value
 print "pga gain: ", params.pga_gain.value
 print "num_pulses", params.num_pulses.value
+print "min_peak_value_th: ", params.sdft_min_peak_value_th.value
 params.sdft_i_min.interface.close_socket()
 
 
