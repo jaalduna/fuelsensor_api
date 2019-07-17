81a6387454e12d60b7f6c5287de45c5b4ae0485a
Aplication <Aplication@MTABCLLOB630.anglo.local>
2019-07-17 16:52:45 -0400

    changes minors

--- src/get_param.py
+++ src/get_param.py
@@ -31,6 +31,7 @@ print "sdft paek: ", params.sdft_peak.value
 print "pga gain: ", params.pga_gain.value
 print "num_pulses", params.num_pulses.value
 print "min_peak_value_th: ", params.sdft_min_peak_value_th.value
+fs.backup_params_to_flash()
 
 params.sdft_i_min.interface.close_socket()
 
