b213de6f0e1b807f7f66818fef11083b8813e785
joaquin aldunate <jaalduna@gmail.com>
2019-08-01 14:06:18 -0400

    added develop_no_eco to test comm bug on app. Also Fuelsensor interface and get_hight_test were updated to support new features. develop_no_eco.hex would respond with version 1.11 and develop.hex would respond with version 1.12

--- src/get_hight_test.py
+++ src/get_hight_test.py
@@ -1,6 +1,6 @@
 import sys
 from Fuelsensor_interface import Fuelsensor_interface
-
+import time
 if len(sys.argv) == 3:
     fs = Fuelsensor_interface(str(sys.argv[1]), int(sys.argv[2]))
 else:
@@ -9,6 +9,16 @@ print "conectando"
 fs.connect()
 print "solicitando altura"
 fs.get_height()
+fs.get_temp()
+fs.get_id()
+fs.get_app_version()
+fs.get_timestamp()
+fs.set_timestamp(00,42,19,0,28,7,19)
+time.sleep(1)
+fs.get_timestamp()
+for i in range(1,100):
+    fs.get_imu_accel_var()
+    time.sleep(0.5)
 #fs.backup_timeseries() # ver porque se pega la respuesta aqui, ver que hay en la interfaz serial o bien debugear el codigo, programando con el pickit 2.
 fs.get_pos()
 fs.close_socket()
